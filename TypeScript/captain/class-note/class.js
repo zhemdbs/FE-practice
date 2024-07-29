7e1f2f9b19255d2df1cbd7329cd9072490621ea9
zhemdbs <zhemdbs@gmail.com>
2024-07-30 00:08:43 +0900

    add. 이넘, 클래스 추가

---
+++ TypeScript/captain/class-note/class.js
@@ -0,0 +1,26 @@
+// class Person {
+//   //클래스 로직
+//   constructor() {
+//     //초기화 메서드
+//     console.log('생성 됨');
+//   }
+// }
+
+// new Person();  //'생성 됨'
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+// var capt = new Person('캡틴', 100)
+
+// class Person {
+//   constructor(name, age) {
+//     console.log('생성 됨');
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// var yoon = new Person('zyoon', 30);  //'생성 됨'
+// console.log(yoon);
