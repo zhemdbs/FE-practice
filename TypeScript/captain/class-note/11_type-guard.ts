9865dff7f00dd59f0c02dedf76a8a63bae296ca8
zhemdbs <zhemdbs@gmail.com>
2024-07-31 21:33:36 +0900

    add. 타입추론, 단언, 가드

---
+++ TypeScript/captain/class-note/11_type-guard.ts
@@ -0,0 +1,27 @@
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function introduce(): Developer | Person {
+  return { name: 'Tony', age: 100, skill: 'Iron Making' }
+}
+
+var tony = introduce();
+console.log(tony.skill); //에러
+console.log(tony.name);
+
+
+if((tony as Developer).skill) {
+  var skill = (tony as Developer).skill;
+  console.log(skill);
+} else if ((tony as Person).age) {
+  var age = (tony as Person).age;
+  console.log(age);
+  
+}
